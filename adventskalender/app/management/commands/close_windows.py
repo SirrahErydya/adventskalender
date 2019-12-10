f7cd400fc72163a7120d68b8ce77a090acf3c3eb
SirrahErydya <kollasch@uni-bremen.de>
2019-12-10 17:15:05 +0100

    Window close command

---
+++ adventskalender/app/management/commands/close_windows.py
@@ -0,0 +1,12 @@
+from django.core.management.base import BaseCommand
+from app.models import Config, Window
+import os
+
+
+class Command(BaseCommand):
+    help = 'Closes all open windows'
+
+    def handle(self, *args, **options):
+        for window in Window.objects:
+            window.open = False
+            window.save()
